5b34c2ddf5b1b7f169bc1580644b2bf723c2553a
gulea <vitalii.g@gbmf.tech>
2024-11-25 13:46:51 +0200

    inital conmit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+        <Text className="text-white">
+          Hello Vlad
+        </Text>
+    </View>
+  );
+}
