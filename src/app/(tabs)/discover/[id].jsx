e2cbd74b29e3ba062e876902f748a4d06c782016
vitalik-gulea <vitalii.g@gbmf.tech>
2025-01-14 20:04:15 +0200

    /

--- src/app/(tabs)/discover/[id].jsx
+++ src/app/(tabs)/discover/[id].jsx
@@ -11,11 +11,19 @@ import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useLocalSearchParams } from "expo-router";
 import { items } from ".";
+import CustomBottomSheet from "../../../components/CustomBottomSheet";
 
 
 
 const Ditails = () => {
   const { id } = useLocalSearchParams();
+    const [isOpenModal, setIsOpenModal] = useState(true)
+    const openModalClick = () => {
+      setIsOpenModal(!isOpenModal)
+    }
+    const setCloseModal = () => {
+      setIsOpenModal(false)
+    }
   useEffect(() => {
     console.log(id, items[id - 1]);
 
@@ -24,13 +32,7 @@ const Ditails = () => {
   return (
     <SafeAreaView className="bg-primary w-full h-full p-4">
       <Image source={items[id - 1].image} className="w-[164px] h-[164px] object-cover rounded-2xl" />
-      {
-        items[id - 1].isBottomSheetActive && (<View>
-          <Text>
-            true
-          </Text>
-        </View>)
-      }
+      <CustomBottomSheet isOpen={isOpenModal} setCloseModal={setCloseModal} />
     </SafeAreaView>
   );
 };
